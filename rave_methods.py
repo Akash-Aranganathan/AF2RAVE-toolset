7023ac5be0a6a60e2650bca0577b038edefb59ad
Akash-Aranganathan <117762898+Akash-Aranganathan@users.noreply.github.com>
2025-09-19 06:03:49 -0400

    initial labelling

--- rave_methods.py
+++ rave_methods.py
@@ -82,6 +82,141 @@ def add_hydrogen(index,forcefield):
     modeller.addHydrogens(forcefield)
     PDBFile.writeFile(modeller.topology, modeller.positions, open(f'fixed_Hydrogen_{index}.pdb', 'w'))
     
+
+def plot_exp_var(pca,n_comp=10):
+  
+  fig = plt.figure(figsize=(5,3));
+  plt.plot([-2,n_comp+2],[0.9,0.9],'--k',alpha=0.4)
+  plt.plot([-2,n_comp+2],[1,1],'--k',alpha=0.4)
+  plt.bar(np.arange(n_comp),pca.explained_variance_ratio_,alpha=0.2,color='g')
+  plt.plot(np.arange(n_comp),pca.explained_variance_ratio_.cumsum(),'--or')
+  plt.ylabel('Explained Variance',fontsize=17)
+  plt.xlabel('Components',fontsize=17)
+  plt.xticks(fontsize=14)
+  plt.yticks(fontsize=14)
+  plt.ylim([0,1.02])
+  plt.xlim([-1,n_comp+1])
+  plt.show()
+
+def plot_proj_labels(projections,proj_labels):
+
+  fig = plt.figure(figsize=(4,4));
+  sca = plt.scatter(projections[::1,0],projections[::1,1],c=proj_labels[::1],marker='.',s=2);
+  cb = fig.colorbar(sca, location='top')
+  cb.set_label('Labels',fontsize=15)
+  plt.xlabel(f'Projection 1',fontsize=18) ;   
+  plt.ylabel(f'Projection 2',fontsize=18);
+  plt.xticks(fontsize=15);
+  plt.yticks(fontsize=15);
+
+
+def get_clusters(projections,colvar_list,num_init_labels,random_seed=10):
+
+  from sklearn.cluster import KMeans
+
+  proj_kmeans = KMeans(n_clusters=num_init_labels,random_state=random_seed).fit(projections)
+  proj_labels = proj_kmeans.labels_
+  unique_labels = np.unique(proj_labels)
+  plot_proj_labels(projections[::10,:2],proj_labels[::10])
+
+  start_ind = 0
+  labels = []
+  one_hot_encoding_labels = np.eye(num_init_labels,dtype=np.int8)[proj_labels]
+
+  for ii,tr in enumerate(colvar_list):
+    stop_ind = start_ind + tr.shape[0]
+    one_hot_per_traj = one_hot_encoding_labels[start_ind:stop_ind,:]
+    labels.append(one_hot_per_traj)
+    start_ind = stop_ind
+
+  return labels
+
+
+def get_tica_labels(colvar_list,num_init_labels,dimensions,time_lag):
+
+  import tqdm
+  from deeptime.decomposition import TICA
+
+  tica = TICA(lagtime=time_lag, dim=dimensions)
+
+  for f in tqdm.tqdm(range(len(colvar_list))):
+    shape = colvar_list[f].shape[0]
+
+    if shape < time_lag + 1:
+        print(f"Skipping {f} with {shape} frames.")
+        continue
+    try:
+        tica.partial_fit((colvar_list[f][:-time_lag,:], colvar_list[f][time_lag:,:]))
+
+    except ValueError:
+
+        print(f"Skipping {f}.")
+
+  tica_output = [tica.transform(colvar_list[k]) for k in range(len(colvar_list))]
+
+  labels = get_clusters(np.concatenate(tica_output),colvar_list,num_init_labels)
+
+  return labels, tica_output
+
+def get_pca_labels(colvar_list,num_init_labels,dimensions):
+
+  from sklearn.decomposition import PCA
+
+  colvars_all = np.concatenate(colvars_list)
+
+  n_comp=10
+  pca = PCA(n_components=n_comp).fit(colvars_all)  # fit the 2-dimensional data
+  plot_exp_var(pca,n_comp=10)
+
+  pca_projection = pca.transform(colvars_all)
+
+  labels = get_clusters(pca_projection[:,:dimensions],colvars_list,num_init_labels)
+
+  pca_output = [pca.transform(colvar_list[k]) for k in range(len(colvar_list))]
+
+  return labels, pca_output
+
+def get_time_labels(colvar_list):
+
+  num_initial_states = len(colvar_list)*2
+  labels = []
+
+  for ii in range(len(colvar_list)):
+
+    lentraj = len(colvar_list[ii])
+    zeroone = np.hstack([np.zeros(int(lentraj/2),dtype=np.int8),np.ones(lentraj-int(lentraj/2),dtype=np.int8)])
+    initlabels = np.eye(num_initial_states)[zeroone+int(ii*2)]
+    labels.append(initlabels)
+
+  print(f"Total number of initial labels: {num_initial_states}\n")
+
+  return labels
+
+
+def create_labels(colvar_file_list,label='tica',num_init_labels=100,time_lag=50,dimensions=2):
+
+  colvar_list = [np.load(ff) for ff in colvar_file_list]
+  
+  if label.upper() not in ['TICA','PCA','TIME']:
+    
+    raise ValueError('not part of ("TICA","PCA","TIME")')
+    return None
+
+  elif label.upper() == 'TICA':
+
+    labels, tica_outputs = get_tica_labels(colvar_list,num_init_labels,dimensions,time_lag)
+    return labels, tica_outputs
+  
+  elif label.upper() == 'PCA':
+
+    labels, pca_outputs = get_pca_labels(colvar_list,num_init_labels,dimensions)
+    return labels, pca_outputs
+  
+  elif label.upper() == 'TIME':
+
+    labels = get_time_labels(colvar_list)
+    return labels
+
 def generate_config(traj_data,initial_labels,dt = 0 ,d = 1,beta =1e-3,learning_rate = 1e-3,encoder_type = "Linear",batch_size = 512,neuron_num1 = 16,neuron_num2 = 16):
     
     f=open('config.ini','w')
